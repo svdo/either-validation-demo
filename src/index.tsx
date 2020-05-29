07728f8be66e2cc9cbc71e2f8eb20526244a35a9
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-05-29 09:57:16 +0200

    Adds semantic-ui-css

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Container>
+      <App />
+    </Container>
   </React.StrictMode>,
   document.getElementById('root')
 )
